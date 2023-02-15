11cafea6ac05ac7d1c4dc89d785d5a5e234d86b7
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-02-15 12:00:46 -0500

    fix jamals package.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import NavBar from './Components/NavBar';
-import { useState } from "react"
+import NavBar from "./Components/NavBar";
+import { useState } from "react";
 
 function App() {
-    }
+  return (
+    <div>
+      <h1>App Up And Running</h1>
+    </div>
+  );
+}
 
 export default App;
